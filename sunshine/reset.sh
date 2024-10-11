a54dd3c77a2e26482cfcd30013015db9ebb25ff1
Dei <dei@deitech.net>
2024-10-11 12:35:04 -0500

    updates

---
+++ sunshine/reset.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env fish
+
+hyprctl dispatch dpms on DP-4
+hyprctl keyword monitor DP-5,3440x1440@144,0x900,1
---
+++ sunshine/set.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env fish
+
+hyprctl dispatch dpms off DP-4
+hyprctl keyword monitor DP-5,1920x1080@120,auto,1
